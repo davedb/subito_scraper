1cb04dbc03e178e095cfd7ff14c44d93df687a66
Davide Galifi <Davide Galifi>
2017-03-29 18:28:22 +0100

    setup api

---
+++ api/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Hapi = require('hapi');
+const scraper = require('./scraper');
+const server = new Hapi.Server();
+
+server.connection({ port: 8080, host: 'localhost' });
+
+
+
+// routes
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply) {
+        reply('The api is working properly!');
+    }
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/search',
+    handler: function(request, reply) {
+        var scrappy = new scraper();
+
+       scrappy.run((res) => {
+            reply(res);
+       });
+    }
+});
+
+// Start server
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
