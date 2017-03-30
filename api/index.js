b79187766c5bbf206139af6cbffdefcb0be8fba5
EUROPE\davideg <davidegalifi@gmail.com>
2017-03-30 11:11:31 +0100

    test change

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ server.route({
     method: 'GET',
     path: '/',
     handler: function(request, reply) {
-        reply('The api is working properly!');
+        reply('Echo API!');
     }
 });
 
