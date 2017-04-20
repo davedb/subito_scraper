0209206bc6f494b6325b863838fe657b73a30f14
EUROPE\davideg <davidegalifi@gmail.com>
2017-04-20 22:03:02 +0100

    DG: test express

--- api/index.js
+++ api/index.js
@@ -1,48 +1,34 @@
 'use strict';
-
-const Hapi = require('hapi');
 const scraper = require('./scraper');
-const server = new Hapi.Server();
 const PythonShell = require('python-shell');
-const pytalk = require('pytalk');
-const utils = require('./lib/utils');
+const utils = require('./core/utils');
 
-server.connection({ port: 8080, host: 'localhost' });
+var express = require('express')
+var app = express()
+
+
 
 // routes
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('The api is working properly!');
-    }
+app.get('/', function (req, res) {
+    res.send('The Api is working properly!')
 });
 
-server.route({
-    method: 'GET',
-    path: '/search',
-    handler: function (request, reply) {
+app.get('/search', function (req, res) {
+    var response = '';
 
-        var response = '';
+    var scrappy = new scraper();
+    var ret = scrappy.run(new PythonShell('../progetto_90_scrapy/main.py'), (message) => {
+        if (message == '###END###') {
+            response = utils.formatScraperArray(response);
 
-        var scrappy = new scraper();
-        var ret = scrappy.run(new PythonShell('../progetto_90_scrapy/main.py'), (message) => {
-            if (message == '###END###') {
-                response = utils.formatScraperArray(response);
+            var objJson = JSON.parse(response);
+            res.send(objJson);
+        }
 
-                var objJson = JSON.parse(response);
-                reply(objJson);
-            }
-
-            response = response + message + ',';
-        });
-    }
+        response = response + message + ',';
+    })
 });
 
-// Start server
-server.start((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`);
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
 });
