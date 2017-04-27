a041d337b53410155322bf131b0ac8289c2f8e69
EUROPE\davideg <davidegalifi@gmail.com>
2017-04-27 16:01:28 +0100

    TDD for search controller - 1

--- api/controllers/search.js
+++ api/controllers/search.js
@@ -3,33 +3,44 @@ var scraper = require('../scraper');
 var PythonShell = require('python-shell');
 var settings = require('../config/settings');
 var utils = require('../core/utils');
+var ValidationException = require('../core/exceptions/ValidationException');
 
 // # Search
 // params:
 // k => keyword
 // items => max items count
 function search(req, res) {
-    var response = '';
-    var keyword = req.query.k;
-    var items = req.query.items;
+        var response = '';
+        var keyword = req.query.k;
+        var items = req.query.items;
 
-    var scrapy = new scraper();
-    var shell = new PythonShell(settings.scrapy_main, {
-        args: ['-s', keyword]
-    });
-
-    var limit = 0;
-    var ret = scrapy.run(shell, (message) => {
-        if (message == '###END###' || limit == items) {
-            response = utils.formatScraperArray(response);
-
-            var objJson = JSON.parse(response);
-            res.send(objJson);
+        // validation
+        if (keyword.trim() === '') {
+            throw new ValidationException('keyword is mandatory');
         }
 
-        response = response + message + ',';
-        limit++;
-    });
+        if(items !== undefined && 
+            (items !== parseInt(items) || items < 1 || items > 100)){
+            throw new ValidationException('items parameter is not valid');
+        }
+
+        var scrapy = new scraper();
+        var shell = new PythonShell(settings.scrapy_main, {
+            args: ['-s', keyword]
+        });
+
+        var limit = 0;
+        var ret = scrapy.run(shell, (message) => {
+            if (message == '###END###' || limit == items) {
+                response = utils.formatScraperArray(response);
+
+                var objJson = JSON.parse(response);
+                res.send(objJson);
+            }
+
+            response = response + message + ',';
+            limit++;
+        });
 }
 
 module.exports = {
--- api/tests/controllers/search-test.js
+++ api/tests/controllers/search-test.js
@@ -1,22 +1,100 @@
 var assert = require('chai').assert;
 var scraper = require('../../scraper');
 var sinon = require('sinon');
+var search = require('../../controllers/search');
 
 describe('# search controller -', () => {
     describe('search -', () => {
-        it('when the keyword parameter is null or empty then it raises an exception', () => {
+        it('when the keyword parameter is empty then it raises an exception', () => {
+            try {
+                var req = {
+                    query: {
+                        k: ''
+                    }
+                };
+                var res = {};
+                search.search(req, res);
+            } catch (err) {
+                assert.equal(err.name, 'ValidationException');
+                assert.equal(err.message, 'keyword is mandatory');
+                return;
+            }
+
+            assert.fail();
+        });
+
+        it('when the keyword parameter is undefined then it raises an exception', () => {
+             try {
+                var req = {
+                    query: {
+                        k: ''
+                    }
+                };
+                var res = {};
+                search.search(req, res);
+            } catch (err) {
+                assert.equal(err.name, 'ValidationException');
+                assert.equal(err.message, 'keyword is mandatory');
+                return;
+            }
+
             assert.fail();
         });
 
         it('when the items parameter is not an integer then it raises an exception', () => {
+             try {
+                var req = {
+                    query: {
+                        k: 'test',
+                        items : 'string'
+                    }
+                };
+                var res = {};
+                search.search(req, res);
+            } catch (err) {
+                assert.equal(err.name, 'ValidationException');
+                assert.equal(err.message, 'items parameter is not valid');
+                return;
+            }
+            
             assert.fail();
         });
 
         it('when the items parameter is lower than 1 then it raises an exception', () => {
+             try {
+                var req = {
+                    query: {
+                        k: 'test',
+                        items : 0
+                    }
+                };
+                var res = {};
+                search.search(req, res);
+            } catch (err) {
+                assert.equal(err.name, 'ValidationException');
+                assert.equal(err.message, 'items parameter is not valid');
+                return;
+            }
+                        
             assert.fail();
         });
 
         it('when the items parameter is greater than 100 then it raises an exception', () => {
+            try {
+                var req = {
+                    query: {
+                        k: 'test',
+                        items : 101
+                    }
+                };
+                var res = {};
+                search.search(req, res);
+            } catch (err) {
+                assert.equal(err.name, 'ValidationException');
+                assert.equal(err.message, 'items parameter is not valid');
+                return;
+            }
+
             assert.fail();
         });
 
